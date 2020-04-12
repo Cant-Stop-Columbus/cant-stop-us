dde06c2e72ff8ab2dec47c6ed3861a474f653b5e
Will Matz <39891237+william-matz@users.noreply.github.com>
2020-04-12 09:05:43 -0400

    Add zoom to map

--- components/Map.js
+++ components/Map.js
@@ -3,7 +3,8 @@ import {
   ComposableMap,
   Geographies,
   Geography,
-  Marker
+  Marker,
+  ZoomableGroup
 } from "react-simple-maps";
 import theme from '../src/theme';
 
@@ -11,8 +12,8 @@ const geoUrl =
 "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";;
 
 const markers = [
-  { markerOffset: 15, name: "", coordinates: [-82.9988, 39.9612] },
-  { markerOffset: 15, name: "", coordinates: [-84.1916, 39.7589] }
+  { markerOffset: 15, name: "Columbus", coordinates: [-82.9988, 39.9612] },
+  { markerOffset: 15, name: "Dayton", coordinates: [-84.1916, 39.7589] }
 ];
 
 const Map = () => {
@@ -24,42 +25,44 @@ const Map = () => {
         scale: 1050
       }}
     >
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies
-            // .filter(d => d.properties.REGION_UN === "Americas")
-            .map(geo => (
-              <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                fill="#EAEAEC"
-                stroke="#D6D6DA"
-              />
-            ))
-        }
-      </Geographies>
-      {markers.map(({ name, coordinates, markerOffset }) => (
-        <Marker key={name} coordinates={coordinates}>
-          <g
-            fill="none"
-            stroke={theme.palette.primary.main}
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            transform="translate(-12, -24)"
-          >
-            <circle cx="12" cy="10" r="3" />
-            <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
-          </g>
-          <text
-            textAnchor="middle"
-            y={markerOffset}
-            style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
-          >
-            {name}
-          </text>
-        </Marker>
-      ))}
+      <ZoomableGroup zoom={1}>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies
+              // .filter(d => d.properties.REGION_UN === "Americas")
+              .map(geo => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill="#EAEAEC"
+                  stroke="#D6D6DA"
+                />
+              ))
+          }
+        </Geographies>
+        {markers.map(({ name, coordinates, markerOffset }) => (
+          <Marker key={name} coordinates={coordinates}>
+            <g
+              fill="none"
+              stroke={theme.palette.primary.main}
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              transform="translate(-12, -24)"
+            >
+              <circle cx="12" cy="10" r="3" />
+              <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+            </g>
+            <text
+              textAnchor="middle"
+              y={markerOffset}
+              style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+            >
+              {name}
+            </text>
+          </Marker>
+        ))}
+      </ZoomableGroup>
     </ComposableMap>
   );
 };
