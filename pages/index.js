30f362bb8904e9af6150fa42d5f1ab097cd18db5
Will Matz <39891237+william-matz@users.noreply.github.com>
2020-04-11 21:22:35 -0400

    Separate toolkit page

--- pages/index.js
+++ pages/index.js
@@ -12,25 +12,13 @@ export default function Index() {
       <Container maxWidth="md">
         <Box mt={2}>
           <Typography variant="h4" align="center">
-            Can't Stop Us Toolkit
+            Can't Stop Us
           </Typography>
           <Typography variant="body2" gutterBottom={true}>
             Can’t Stop Columbus is a community-wide movement that activates our city’s talents and compassion to solve real problems. We embrace courageous ideas and rapidly deliver new and innovative solutions. We built this toolkit to help organize the movement in other cities.
           </Typography>
-          <Typography variant="h5">
-            The purpose of this toolkit is:
-          </Typography>
-          <Typography variant="body1">
-            <b>To help</b> other communities kickstart support movements like Can’t Stop Columbus
-          </Typography>
-          <Typography variant="body1">
-            <b>To empower</b>  organizers to inspire an attitude of help and support
-          </Typography>
-          <Typography variant="body1" gutterBottom={true}>
-            <b>To provide</b>  a structure for creating a movement by sharing tools, insights, successes, and failures
-          </Typography>
-          <Button variant="contained" color="primary" component={Link} href="https://docs.google.com/document/d/1H72twsb5stdXdGf3Gl0oO7fsPYhU9ZLKEr1-jQiX2iU/edit?usp=sharing">
-            Open the Toolkit
+          <Button variant="contained" color="primary" component={Link} href="/toolkit">
+            Toolkit
           </Button>
         </Box>
       </Container>
---
+++ pages/toolkit.js
@@ -0,0 +1,36 @@
+import Container from '@material-ui/core/Container'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+import { Button } from '@material-ui/core'
+import Link from '@material-ui/core/Link'
+import Header from '../components/Header';
+
+export default function Index() {
+  return (
+    <>
+      <Header/>
+      <Container maxWidth="md">
+        <Box mt={2}>
+          <Typography variant="h4" align="center">
+            Can't Stop Us Toolkit
+          </Typography>
+          <Typography variant="h5">
+            The purpose of this toolkit is:
+          </Typography>
+          <Typography variant="body1">
+            <b>To help</b> other communities kickstart support movements like Can’t Stop Columbus
+          </Typography>
+          <Typography variant="body1">
+            <b>To empower</b>  organizers to inspire an attitude of help and support
+          </Typography>
+          <Typography variant="body1" gutterBottom={true}>
+            <b>To provide</b>  a structure for creating a movement by sharing tools, insights, successes, and failures
+          </Typography>
+          <Button variant="contained" color="primary" component={Link} href="https://docs.google.com/document/d/1H72twsb5stdXdGf3Gl0oO7fsPYhU9ZLKEr1-jQiX2iU/edit?usp=sharing">
+            Open the Toolkit
+          </Button>
+        </Box>
+      </Container>
+    </>
+  );
+}
